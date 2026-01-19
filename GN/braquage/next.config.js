c94c60729bdbbcffcc590728549450ea1ef6fa3f
Nathalie Marsan <nmarsan@voyageprive.com>
2026-01-19 12:29:16 +0100

    fix

--- GN/braquage/next.config.js
+++ GN/braquage/next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  // Include data directory in output tracing for standalone mode
-  outputFileTracingIncludes: {
-    '/*': ['./data/**/*'],
-  },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     return config;
