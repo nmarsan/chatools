d8f1ff695f093e517b57039299a262dd0d8f2a83
Nathalie Marsan <nmarsan@voyageprive.com>
2026-01-19 14:27:30 +0100

    fix

--- GN/braquage/next.config.js
+++ GN/braquage/next.config.js
@@ -1,9 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   webpack: (config) => {
     config.resolve.alias.canvas = false;
+    
+    // Explicitly resolve @ alias to project root
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    
     return config;
   },
 }
